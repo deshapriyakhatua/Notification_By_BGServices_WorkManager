f10b44e930c0c833bdadec4bc8b4bb2496f120fb
deshapriya <deshapriyakhatua@gmail.com>
2022-10-25 19:20:46 +0530

    work_manager_and_service_clean

--- app/src/main/java/com/example/coursenotification/WorkManager.java
+++ app/src/main/java/com/example/coursenotification/WorkManager.java
@@ -1,35 +1,13 @@
 package com.example.coursenotification;
 
-import static android.content.Context.NOTIFICATION_SERVICE;
-
-
-import android.app.Notification;
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
 import android.content.Context;
-import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.media.MediaPlayer;
-import android.net.Uri;
 import android.provider.Settings;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.work.Worker;
 import androidx.work.WorkerParameters;
-
-import com.android.volley.AuthFailureError;
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
-
-import java.util.HashMap;
-import java.util.Map;
 
 public class WorkManager extends Worker {
 
