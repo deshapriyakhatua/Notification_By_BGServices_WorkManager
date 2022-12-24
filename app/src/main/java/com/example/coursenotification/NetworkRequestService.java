2648dcebc675b5dff3519044c61d5ed123411ce7
deshapriya <deshapriyakhatua@gmail.com>
2022-12-24 19:07:50 +0530

    24 12 2022

--- app/src/main/java/com/example/coursenotification/NetworkRequestService.java
+++ app/src/main/java/com/example/coursenotification/NetworkRequestService.java
@@ -36,18 +36,8 @@ import java.util.concurrent.TimeUnit;
 
 public class NetworkRequestService extends Service {
 
-    private NotificationManager notificationManager;
-    private Notification notification;
-    private Notification notification2;
-    private Notification notification3;
-    private RequestQueue queue;
-    private StringRequest stringRequest;
-    private static final int REQUEST_CODE = 1;
-    private static int NOTIFICATION_ID = 1;
-    private static String NOTIFICATION_CHANNEL_ID = "1";
 
     private static Runnable runnable;
-    private String requestedResult;
     MediaPlayer mediaPlayer;
     ScheduledExecutorService scheduledExecutorService;
 
@@ -68,7 +58,6 @@ public class NetworkRequestService extends Service {
             public void run() {
                 // TODO Auto-generated method stub
                 mediaPlayer.start();
-                queue.add(stringRequest);
 
             }
         },10,30, TimeUnit.SECONDS);
